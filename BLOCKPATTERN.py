d139c6e83bbcc212c7ba63674c0e3ce5adfbd57a
bhavya110706 <147322057+bhavya110706@users.noreply.github.com>
2025-08-18 22:16:44 +0530

    Add files via upload

---
+++ BLOCKPATTERN.py
@@ -0,0 +1,13 @@
+star = int(input("enter no of stars: "))
+block = int(input("enter no of blocks: "))
+line = int(input("enter no of lines: "))
+
+for i in range(block):
+    count = 0  
+    for j in range(line - i):  
+        for k in range(star):
+            print("*", end=" ")
+            count += 1
+        print() 
+    print(count)  
+    print("_____________")
